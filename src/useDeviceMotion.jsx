6a843648b4d59e31007bda85df39ea6513e3285f
MaTTalv001 <pomeranian1981@gmail.com>
2024-04-26 20:18:36 +0900

    punch

--- src/useDeviceMotion.jsx
+++ src/useDeviceMotion.jsx
@@ -5,33 +5,33 @@ function useDeviceMotion() {
     const [permissionGranted, setPermissionGranted] = useState(false);
 
     useEffect(() => {
-        function handleMotionEvent(event) {
+        const handleMotionEvent = event => {
             setMotion({
                 x: event.accelerationIncludingGravity.x,
                 y: event.accelerationIncludingGravity.y,
                 z: event.accelerationIncludingGravity.z
             });
-        }
+        };
 
         if (permissionGranted) {
             window.addEventListener('devicemotion', handleMotionEvent);
-            return () => {
-                window.removeEventListener('devicemotion', handleMotionEvent);
-            };
+            return () => window.removeEventListener('devicemotion', handleMotionEvent);
         }
     }, [permissionGranted]);
 
-    const requestPermission = () => {
+    const requestPermission = async () => {
         if (typeof DeviceMotionEvent.requestPermission === 'function') {
-            DeviceMotionEvent.requestPermission()
-                .then(permissionState => {
-                    if (permissionState === 'granted') {
-                        setPermissionGranted(true);
-                    } else {
-                        alert('Permission not granted');
-                    }
-                })
-                .catch(console.error);
+            try {
+                const permissionState = await DeviceMotionEvent.requestPermission();
+                if (permissionState === 'granted') {
+                    setPermissionGranted(true);
+                } else {
+                    alert('Permission not granted');
+                }
+            } catch (error) {
+                console.error('Permission request failed', error);
+                alert('Error requesting permission.');
+            }
         } else {
             alert('DeviceMotionEvent.requestPermission is not supported on this device.');
         }
