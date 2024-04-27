268e6f35bf63e17ca4b5fffb8e488993aea00b05
MaTTalv001 <pomeranian1981@gmail.com>
2024-04-27 14:46:03 +0900

    ranking

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
+  const stopPropagation = (e) => {
+    e.stopPropagation(); // イベントの伝播を停止
+  };
+
   return (
-    <div className="modal-backdrop">
-      <div className="modal-content">
-                {children}
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={stopPropagation}>
+        {children}
         <button className="close-button" onClick={onClose}>閉じる</button>
-              </div>
+      </div>
     </div>
   );
 };
+
 
 export default Modal;
